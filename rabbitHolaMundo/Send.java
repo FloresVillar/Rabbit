486e5a73a3d0f0b6c285455b19aca2fd85293cad
Flores Villar <efloresv@uni.pe>
2025-06-02 16:44:19 -0500

    saludo

---
+++ rabbitHolaMundo/Send.java
@@ -0,0 +1,23 @@
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class Send{
+    private final static String COLA_DE_MENSAJES = "saludo";
+    public static void main(String[]argv) throws Exception{
+        ConnectionFactory creador = new ConnectionFactory();
+        creador.setHost("localHost");
+        try(
+            Connection conexion = creador.newConnection();
+            Channel canal = conexion.createChannel();
+        ){
+            canal.queueDeclare(COLA_DE_MENSAJES,false,false,false,null);//creando
+            String mensaje = "Hello World";
+            canal.basicPublish("",COLA_DE_MENSAJES,null,mensaje.getBytes(StandardCharsets.UTF_8));//publicando el mensaje
+            System.out.println("[x] enviando '"+mensaje+"'");
+        }
+    }
+
+}
